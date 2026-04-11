4f17c11e16f991e2b7f43b7a707d7f388d5f6b26
jeanfourieryao <jeanfourieryao@gmail.com>
2026-04-11 01:37:49 +0000

    Update cached asset from index.html to index-3.html

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'monsouclou-v1';
-const ASSETS = ['./', './index.html',
+const ASSETS = ['./', './index-3.html',
   'https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600;700&family=Fraunces:ital,wght@0,700;0,900;1,700&display=swap'];
 
 self.addEventListener('install', e => {
@@ -13,7 +13,7 @@ self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(cached => cached || fetch(e.request)
       .then(resp => { const c = resp.clone(); caches.open(CACHE).then(ca=>ca.put(e.request,c)); return resp; })
-      .catch(() => caches.match('./index.html'))
+      .catch(() => caches.match('./index-3.html'))
     )
   );
 });
